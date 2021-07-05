e8676066453dfcca67d95b9eee5413fed9eb9912
Remko Duursma <remko@shintolabs.nl>
2021-07-05 15:44:39 +0200

    dontuse

--- R/wrappers.R
+++ R/wrappers.R
@@ -241,4 +241,29 @@ dashboard_page <- function(header, sidebar, body, ...){
 
 
 
-
+#'
+#' #' Modal dialog with BS4 settings
+#' #' @export
+#' modal_dialog <- function (..., title = NULL, footer = modalButton("Dismiss"),
+#' size = c("m", "s", "l"), easyClose = FALSE,
+#' fade = TRUE)
+#' {
+#'   size <- match.arg(size)
+#'   cls <- if (fade)
+#'     "modal fade"
+#'   else "modal"
+#'   div(id = "shiny-modal", class = cls, tabindex = "-1",
+#'       `data-backdrop` = if (!easyClose)
+#'         "static", `data-keyboard` = if (!easyClose)
+#'           "false", div(class = "modal-dialog",
+#'                        class = switch(size, s = "modal-sm", m = NULL,
+#'                                       l = "modal-lg"), div(class = "modal-content",
+#'                                                            if (!is.null(title))
+#'                                                              div(class = "modal-header", tags$h4(class = "modal-title",
+#'                                                                                                  title)), div(class = "modal-body",
+#'                                                                                                               ...), if (!is.null(footer))
+#'                                                                                                                 div(class = "modal-footer", footer))),
+#'       tags$script("$('#shiny-modal').modal().focus();"))
+#' }
+#'
+#'
